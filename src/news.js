055c4722d70b118e3ef7c5708451512888e4b73e
RanaElsekhily <ranaelsekhily@gmail.com>
2022-07-30 22:44:10 +0200

    Delete src folder

--- src/news.js
+++
@@ -1,8 +0,0 @@
-const url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json";
-//"https://newsapi.org/v2/everything?q=tesla&from=2021-12-09&sortBy=publishedAt&apiKey=69748a26a56f4d7c884b2271041db0b3";
-
-export async function getNews() {
-    let result = await fetch(url).then(response => response.json());
-    console.log('result');
-    return result.articles;
-}
